5a2cbb4a7de51b15d1ce7611210fe774eb1f97c0
Sameer Khan <sameerkhann241@gmail.com>
2024-09-24 13:06:34 +0530

    Backend Commit

--- index.js
+++ index.js
@@ -7,17 +7,20 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
+import path from "path"
 
 dotenv.config({});
 
 const app = express();
+
+const _dirname = path.resolve();
 
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://joblocator.netlify.app/',
+    origin:'http://localhost:5173',
     credentials:true
 }
 
@@ -32,9 +35,14 @@ app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
+app.get('*', (_,res) =>{
+    res.sendFile(path.resolve(__dirname, "frontend", "dust", "index.html"))
+})
+
 
 
 app.listen(PORT,()=>{
     connectDB();
-    console.log(`Server running at port ${PORT}`);
+    // console.log(`Server running at port ${PORT}`);
 })
