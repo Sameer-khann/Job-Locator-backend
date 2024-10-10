95a658e127948b32d9ed26dcc724261b18d66277
Sameer Khan <sameerkhann241@gmail.com>
2024-10-10 16:23:22 +0530

    Trying to fix Auth issue in Production

--- index.js
+++ index.js
@@ -18,12 +18,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
+
 const corsOptions = {
     origin:'https://joblocator.netlify.app',
     // origin:"http://localhost:5173",
-    credentials:true
+    credentials:true,
 }
-
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
